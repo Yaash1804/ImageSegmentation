3ac2def0a294a07b284c1457df89bb25dbe67f3b
Yaash1804 <156991731+Yaash1804@users.noreply.github.com>
2024-10-06 00:31:47 +0530

    Update segmentation_sample.py

--- scripts/segmentation_sample.py
+++ scripts/segmentation_sample.py
@@ -27,7 +27,7 @@ from guided_diffusion.script_util import (
     args_to_dict,
 )
 import torchvision.transforms as transforms
-from torchsummary import summary
+#from torchsummary import summary
 seed=10
 th.manual_seed(seed)
 th.cuda.manual_seed_all(seed)
